23b36e807fa5f7b37cf7f4f923df5f301c8c900e
jammit <james.mitsumasu@gmail.com>
2025-08-05 21:30:20 -0500

    added rotation input

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 
 from constants import *
 
+from player import Player
+
 
 def main():
     pygame.init()
@@ -14,12 +16,16 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
 
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
         dt  =   clock.tick(60) / 1000
 
