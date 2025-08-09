5fd3708150922100586ee1502dd9e772e4276f62
jammit <james.mitsumasu@gmail.com>
2025-08-08 23:01:16 -0500

    rate limited shooting

--- player.py
+++ player.py
@@ -9,6 +9,7 @@ class Player(CircleShape):
   def __init__(self, x, y):
     super().__init__(x, y, PLAYER_RADIUS)
     self.rotation = 0
+    self.timer = 0
 
   # in the player class
   def triangle(self):
@@ -31,6 +32,7 @@ class Player(CircleShape):
   def shoot(self):
     shot = Shot(self.position.x, self.position.y, SHOT_RADIUS)
     shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+    self.timer = PLAYER_SHOOT_COOLDOWN
 
 
   def draw(self, screen):
@@ -39,6 +41,13 @@ class Player(CircleShape):
 
   def update(self, dt):
       keys = pygame.key.get_pressed()
+      print(self.timer)
+
+      if self.timer > 0:
+        self.timer -= dt
+
+      if self.timer < 0:
+        self.timer = 0
 
       if keys[pygame.K_a]:
         self.rotate(-dt)
@@ -52,4 +61,5 @@ class Player(CircleShape):
         self.move(-dt)
 
       if keys[pygame.K_SPACE]:
-        self.shoot()
+        if self.timer == 0:
+          self.shoot()
